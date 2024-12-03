20a3698ea82aedf8b6eace22ab9bc4af5b5b135e
nika-joyboy <00001108115251sp@al.educacao.sp.gov.br>
2024-12-03 15:47:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Engenharia',
+    'O que é Engenharia?',
+    'Engenharia é o campo do conhecimento que aplica princípios científicos e matemáticos para projetar, construir e melhorar infraestruturas, máquinas, processos e sistemas.'
+)
+
+criaCartao(
+    'Engenharia',
+    'Quais são as principais áreas da Engenharia?',
+    'As principais áreas incluem Engenharia Civil, Mecânica, Elétrica, Química, de Produção, Ambiental, Computação, entre outras.'
+)
+
+criaCartao(
+    'Engenharia',
+    'Qual a diferença entre Engenharia e Arquitetura?',
+    'A Engenharia foca na parte técnica de projetos, como cálculos estruturais e sistemas, enquanto a Arquitetura se concentra no design e funcionalidade estética dos espaços.'
+)
+
+criaCartao(
+    'Engenharia',
+    'O que é um engenheiro de produção?',
+    'O engenheiro de produção é responsável por otimizar processos produtivos, buscando melhorar a eficiência, reduzir custos e aumentar a qualidade em fábricas e empresas.'
+)
